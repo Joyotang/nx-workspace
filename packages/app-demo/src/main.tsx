cba6f762c4fc3ac2a6966cb9706ec55be223cae1
Joyo Tang <joyo.tang@kinochat.ai>
2026-05-21 16:42:37 +0800

    feat: add tanstack-router and tailwindcss and shadcn

--- packages/app-demo/src/main.tsx
+++ packages/app-demo/src/main.tsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/globals.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+import {
+  RouterProvider,
+} from '@tanstack/react-router'
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import {
+  ReactQueryDevtools,
+} from '@tanstack/react-query-devtools'
+
+import { router } from './router'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
